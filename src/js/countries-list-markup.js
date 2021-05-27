631d1e6e559afa8bf33ef57e255346f6be575966
NOS-DONE <trombonesterenko@gmail.com>
2021-05-27 17:56:16 +0200

    added clickable list-items

--- src/js/countries-list-markup.js
+++ src/js/countries-list-markup.js
@@ -1,5 +1,9 @@
 import countriesListTemplate from '../templates/countries-list.hbs';
+import renderOneCountry from '../js/one-country-murkup.js';
+import fetchCountries from './fetchCountries.js'
+
 import { alert, notice, info, success, error } from "@pnotify/core";
+import { func } from 'assert-plus';
 
 
 const containerRef = document.querySelector('.container');
@@ -11,6 +15,7 @@ export default function renderCountriesList(arr) {
     if (arr.length > 1 && arr.length <= 10) {
 
         containerRef.innerHTML = murkup;
+        linkCountry()
         return;
     }
     
@@ -20,7 +25,25 @@ export default function renderCountriesList(arr) {
             title: "To many mathces found. Please enter a more specific query!",
         })
     }
- 
+}
 
+function linkCountry() {
+    const countryItemRef = document.querySelector('.country-list');
+    countryItemRef.addEventListener('click', onCountryClick)
+}
+
+function onCountryClick(event) {
+    console.dir(event.target.dataset.code);
+    console.dir(event.target);
+    const countryCode = event.target.dataset.code;
+
+    console.log(countryCode);
+    if (event.target.nodeName !== 'UL') {
+        fetchCountries(event.target.textContent)
+            .then(responce => {
+                console.log(responce);
+                renderOneCountry(responce.filter(country => country.alpha3Code === countryCode))
+            })
+    }
 
 }
--- src/js/one-country-murkup.js
+++ src/js/one-country-murkup.js
@@ -4,6 +4,7 @@ const containerRef = document.querySelector('.container');
 
 export default function renderOneCountry(arr) {
 
+
     const murkup = oneCountryTemplate(...arr);
 
     if (arr.length === 1) {
